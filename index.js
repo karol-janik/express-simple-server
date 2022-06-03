f1224261a01b4527c8dbdb3baad1bc4b6c41860e
Adam Misiek <61427836+QaiZeyn@users.noreply.github.com>
2022-06-03 13:05:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ server.use(express.static('public'));
 server.get('/', (_req, res) => {
   res.send('Hello Express!');
 });
-
 server.get('/fetch-wordpress-graphql', async (_req, res) => {
   const query = `
     query QueryPosts {
