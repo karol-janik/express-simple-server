7b2ed86633ae1527c2fc4988bd1d6271cbb8dcc7
Adrian Markowski <amarkowski@student.agh.edu.pl>
2022-07-15 12:45:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 3300;
 
-//server.use(express.static('public'));
+server.use(express.static('public'));
 
 server.get('/', (_req, res) => {
   res.send('Hello Expresshhhh!');
